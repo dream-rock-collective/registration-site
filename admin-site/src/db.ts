d82beb657cf31d1392a0580b001492678a617157
garrepi <jacgarrett18@gmail.com>
2026-08-11 14:53:45 -0700

    split

--- admin-site/src/db.ts
+++
@@ -1,8 +0,0 @@
-import postgres from "postgres";
-
-// The connection string comes from Docker in production and can be overridden
-// locally when running the API directly.
-export const database = postgres(
-  process.env.DATABASE_URL ??
-    "postgres://admin:admin@localhost:5432/collective_admin",
-);
