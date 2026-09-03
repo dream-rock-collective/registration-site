08132ae30d30f4c359b21d6e29495405f2223533
garrepi <jacgarrett18@gmail.com>
2026-09-03 16:23:19 -0700

    design thanks page

--- src/newsletter-thanks.ts
+++ src/newsletter-thanks.ts
@@ -1,1 +1,16 @@
 import "./style.css";
+import { getMember } from "./member";
+
+const heading = document.querySelector<HTMLElement>(
+  "#newsletter-thanks-heading",
+);
+const bannerHeading = document.querySelector<HTMLElement>(
+  "#newsletter-thanks-banner",
+);
+const member = getMember();
+
+if (heading && member?.name) {
+  const firstName = member.name.trim().split(/\s+/)[0];
+  bannerHeading?.replaceChildren(`Thanks ${firstName}!`);
+  heading.textContent = `Thank you, ${firstName}!`;
+}
